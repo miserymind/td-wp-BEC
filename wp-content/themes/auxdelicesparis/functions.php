7650bd7af3dbe0a66d884b8fd832ef346f053b69
card1004 <carole.carduner@gmail.com>
2014-06-02 12:56:57 +0200

    Ajout Woo

--- wp-content/themes/auxdelicesparis/functions.php
+++ wp-content/themes/auxdelicesparis/functions.php
@@ -20,7 +20,7 @@ function codex_patisserie_init() {
         'all_items'          => __( 'Toutes les Pâtisseries', 'your-plugin-textdomain' ),
         'search_items'       => __( 'Rechercher une Pâtisserie', 'your-plugin-textdomain' ),
         'parent_item_colon'  => __( 'Parent Pâtisseries:', 'your-plugin-textdomain' ),
-        'not_found'          => __( 'Aucunes Pâtisseries trouver', 'your-plugin-textdomain' ),
+        'not_found'          => __( 'Aucunes Pâtisseries trouvé', 'your-plugin-textdomain' ),
         'not_found_in_trash' => __( 'Aucunes Pâtisseries dans la poubelle', 'your-plugin-textdomain' ),
     );
 
@@ -43,11 +43,41 @@ function codex_patisserie_init() {
 }
 
 
-add_filter('gettext', 'change_post_to_article');
-add_filter('ngettext', 'change_post_to_article');
-function change_post_to_article($translated) {
-    $translated = str_ireplace('Utilisateur', 'Pâtissier', $translated);
-    return $translated;
+add_action( 'init', 'codex_patissier_init' );
+function codex_patissier_init() {
+    $labels = array(
+        'name'               => _x( 'Pâtissiers', 'post type general name', 'your-plugin-textdomain' ),
+        'singular_name'      => _x( 'Pâtissier', 'post type singular name', 'your-plugin-textdomain' ),
+        'menu_name'          => _x( 'Pâtissiers', 'admin menu', 'your-plugin-textdomain' ),
+        'name_admin_bar'     => _x( 'Pâtissier', 'add new on admin bar', 'your-plugin-textdomain' ),
+        'add_new'            => _x( 'Ajouter un nouveau Pâtissier', 'patisserie', 'your-plugin-textdomain' ),
+        'add_new_item'       => __( 'Ajouter un nouveau Pâtissier', 'your-plugin-textdomain' ),
+        'new_item'           => __( 'Nouveau Pâtissier', 'your-plugin-textdomain' ),
+        'edit_item'          => __( 'Modifier un Pâtissier', 'your-plugin-textdomain' ),
+        'view_item'          => __( 'Regarder le Pâtissier', 'your-plugin-textdomain' ),
+        'all_items'          => __( 'Tous les Pâtissiers', 'your-plugin-textdomain' ),
+        'search_items'       => __( 'Rechercher un Pâtissier', 'your-plugin-textdomain' ),
+        'parent_item_colon'  => __( 'Parent Pâtissiers:', 'your-plugin-textdomain' ),
+        'not_found'          => __( 'Aucuns Pâtissier trouvé', 'your-plugin-textdomain' ),
+        'not_found_in_trash' => __( 'Aucuns Pâtissiers dans la poubelle', 'your-plugin-textdomain' ),
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'patissier' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => null,
+        'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt')
+    );
+
+    register_post_type( 'patissier', $args );
 }
 
 
