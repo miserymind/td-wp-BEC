591451bbc07867ffc8cb7f59e55732217f14301e
card1004 <carole.carduner@gmail.com>
2014-06-03 10:50:03 +0200

    Update Css

--- wp-content/themes/auxdelicesparis/header.php
+++ wp-content/themes/auxdelicesparis/header.php
@@ -10,13 +10,15 @@
         <meta name="keywords" content="slit slider, plugin, css3, transitions, jquery, fullscreen, autoplay" />
         <meta name="author" content="Codrops" />
         <link rel="shortcut icon" href="../favicon.ico">
-        <!--Bootstrap-->
+        <!-- Bootstrap -->
         <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css" rel="stylesheet">
         <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.2/css/font-awesome.min.css" rel="stylesheet">
-        <!-- Less-->
+        <!-- Less -->
         <link rel="stylesheet/less" type="text/css" href="<?php bloginfo('template_url'); ?>/css/style.less" media="all" />
         <script type='text/javascript' src="<?php bloginfo('template_url'); ?>/js/less-1.1.3.min.js"></script>
         <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_url'); ?>/css/style.css" />
+        <!-- Font -->
+        <link href='http://fonts.googleapis.com/css?family=Devonshire' rel='stylesheet' type='text/css'>
        
     </head>
 
@@ -40,7 +42,7 @@
                 </div>
                 <div class="col-md-3"></div>
                 <div class="col-md-6 welcome">
-                    <?php the_field('mot_header') ?>
+                    <p><?php the_field('mot_header') ?></p>
                 </div>
                 <div class="col-md-3"></div>
             </div>
--- wp-content/themes/auxdelicesparis/template-home.php
+++ wp-content/themes/auxdelicesparis/template-home.php
@@ -5,10 +5,24 @@ Template Name: Homepage
 
 get_header(); ?>
 
-    <div class="row">
-        <div class="container">
-            <div class="col-md-12">
-                <?php the_field('acf_mot-header') ?>
+    <div class="row homepage">
+        <div class="concept">
+            <div class="container">
+                <h1>Le concept</h1>
+                <div class="col-md-12">
+                    <div class="col-md-4">
+                        <img src="<?php bloginfo('template_url'); ?>/images/conceptImgLeft.png" alt=""/>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="<?php bloginfo('template_url'); ?>/images/conceptImgCenter.png" alt=""/>
+                    </div>
+                    <div class="col-md-4">
+                        <img src="<?php bloginfo('template_url'); ?>/images/conceptImgRight.png" alt=""/>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <p><?php the_field('le_concept') ?></p>
+                </div>
             </div>
         </div>
     </div>
